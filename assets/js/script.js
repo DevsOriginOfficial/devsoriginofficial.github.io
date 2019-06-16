ba39ee83f6ef42e09d73b88c39d2ecc33a34c6f3
Md. Jamal Uddin <jamal.pb95@gmail.com>
2019-06-17 00:50:17 +0600

    initial project setup for v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,6 @@
-(function($) {
-  "use strict"
+'use strict';
 
-  //for swiper slider
-  new Swiper('.swiper-container', {
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  });
-
-  //for smooth scroll
-  $('html').smoothScroll(300);
-
-  //for jump_up button
-  $(window).scroll(function () {
-      const heroHeight = $('.navbar_slider_section').outerHeight(true);
-      const scrolltop = $(window).scrollTop();
-
-      if (scrolltop > heroHeight) {
-          $('.jump_up').css('display', 'inline-block');
-      } else {
-          $('.jump_up').css('display', 'none');
-      }
-  });
-
-})(jQuery);
+(function() {
+	// eslint-disable-next-line no-console
+	console.log('Hello World!');
+})();
