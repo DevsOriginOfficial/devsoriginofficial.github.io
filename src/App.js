5d901bf68f2f0a25236740824c23b6d7c4741f3b
Md. Jamal Uddin <jamal.pb95@gmail.com>
2019-07-01 22:57:34 +0600

    add button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello DevsOrigin</h1>
+			<Button />
 		</div>
 	);
 }
